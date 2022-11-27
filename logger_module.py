15c3a9903fc4e754948c4c4087a7efbfc6c1cfdb
Kari Noriy <knoriy72@gmail.com>
2022-11-27 20:51:17 +0000

    add: added code for python logging

--- logger_module.py
+++ logger_module.py
@@ -1,9 +1,16 @@
 import logging
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 LOG_FORMAT = "%(name)s : %(levelname)s : %(asctime)s : %(message)s"
-logging.basicConfig(filename="my_logs.log" ,level=logging.INFO, format=LOG_FORMAT)
+formatter = logging.Formatter(LOG_FORMAT)
+
+file_handler = logging.FileHandler('my_logs.log')
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+
 
 logger.info(f"info about my my_module_name")
 logger.warning(f"warning message from my my_module_name")
